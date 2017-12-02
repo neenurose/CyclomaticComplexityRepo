ffc68851d7bddd0779d373d01ca5198e03a679b5
neenurose <neenuvincent1992@gmail.com>
2017-12-02 19:21:21 +0000

    bug fixed - related to locking.

--- Master.py
+++ Master.py
@@ -17,24 +17,27 @@ class master:
     def POST(self):
         # get the passed parameters host and port from the url
         worker_details = web.input(host='',port='')
+        web.config.lock.acquire()
         if web.config.pointer <= len(web.config.commit_files):
-            web.config.lock.acquire()
             # get the git commit hex and filename from commit_files dictionary
             #print(web.config.pointer)
             (commithex,filename) = web.config.commit_files[web.config.pointer]
             #print((commithex,filename))
+            web.config.pointer = web.config.pointer+1
+            web.config.lock.release()
             # call the worker webservice for doing work by passing commit hex and filename
             url = "http://"+worker_details.host+":"+worker_details.port+"/worker?commithex="+commithex+"&filename="+filename
             print(url)
-            web.config.pointer = web.config.pointer+1
-            web.config.lock.release()
             print("pointer: ",web.config.pointer)
+            print("length: ",len(web.config.commit_files))
             response = req.get(url)
             return "Done"
 
 
         else:
+            web.config.lock.release()
             return "No task to assign!"
+
 
 
 
@@ -55,7 +58,7 @@ class done_work:
         web.config.lock.release()
         web.config.result_sum = web.config.result_sum + worker_result.result
         print("counter: ",web.config.counter)
-        if web.config.counter == (web.config.pointer):
+        if web.config.counter == len(web.config.commit_files)-2:
             complexity_avg = web.config.result_sum/web.config.counter
             print("Average", complexity_avg)
         return "Work done!"
@@ -90,6 +93,7 @@ if __name__=="__main__":
             if os.path.splitext(filename)[1] not in [".txt",".csv",".pdf",".md","",".pyc"]:
                 web.config.commit_files[i+1] = (each_commit.hexsha,filename)
                 i=i+1
+    print(web.config.commit_files)
     print(len(web.config.commit_files))
 
 
